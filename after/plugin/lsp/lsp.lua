f8a85218fac64fec60da145d3655bf64335931a9
Adrian Brady <ajbrady999@protonmail.com>
2023-10-05 21:20:59 -0500

    Add miniclue and change keybinds

--- after/plugin/lsp/lsp.lua
+++ after/plugin/lsp/lsp.lua
@@ -33,6 +33,7 @@ local filename = vim.fn.expand('%:t:r')
 local cmp = require('cmp')
 
 cmp.setup({
+    preselect = cmp.PreselectMode.None,
     sources = {
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
@@ -51,10 +52,22 @@ cmp.setup({
     }
 })
 
+local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+cmp.event:on(
+  'confirm_done',
+  cmp_autopairs.on_confirm_done()
+)
+
 
 
 lsp.set_preferences({
 	sign_icons = { }
+})
+
+require('mini.clue').setup({
+  clues = {
+    { mode = 'n', keys = '<Leader>l', desc = '+LSP' },
+  },
 })
 
 lsp.on_attach(function(client, bufnr)
@@ -63,13 +76,13 @@ lsp.on_attach(function(client, bufnr)
 
     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts, { desc = 'goto definition' })
     vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts, { desc = 'hov menu' })
-    vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
-    vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
-    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
-    vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, opts)
-    vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, opts)
-    vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, opts)
+    vim.keymap.set("n", "<leader>lws", function() vim.lsp.buf.workspace_symbol() end, opts, { desc = 'workspace symbol' })
+    vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float() end, opts, { desc = 'open diagnostics' })
+    vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts, { desc = 'next diagnostic' })
+    vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts, { desc = 'prev diagnostic' })
+    vim.keymap.set("n", "<leader>lca", function() vim.lsp.buf.code_action() end, opts, { desc = 'code action' })
+    vim.keymap.set("n", "<leader>lrr", function() vim.lsp.buf.references() end, opts)
+    vim.keymap.set("n", "<leader>lrn", function() vim.lsp.buf.rename() end, opts)
     vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, opts)
     require('mini.clue').setup({})
 
