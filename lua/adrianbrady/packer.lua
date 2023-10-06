c4cc3d94680f2436d44589331f85e261bfe4b4f2
Adrian Brady <ajbrady999@protonmail.com>
2023-10-06 13:57:14 -0500

    Add lazygit integration
    Added utils folder in /lua/utils to be able to create custom modules, used for calling custom functions

--- lua/adrianbrady/packer.lua
+++ lua/adrianbrady/packer.lua
@@ -7,6 +7,10 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
+    use {"akinsho/toggleterm.nvim", tag = '*', config = function()
+        require("toggleterm").setup()
+    end}
+
     use 'mhinz/vim-startify'
 
     use {
