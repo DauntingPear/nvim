b2ecca47be53eeeafb0beaf36968ab8e88deb1b9
Adrian Brady <ajbrady999@protonmail.com>
2023-10-07 20:58:21 -0500

    remove packer check and add lazy check

--- lua/adrianbrady/packer.lua
+++ lua/adrianbrady/packer.lua
@@ -1,7 +1,15 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
--- Only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
 
 return require('packer').startup(function(use)
     -- Packer can manage itself
