3e278814653592c88e95234122ac2258da3f5a58
Adrian Brady <ajbrady999@protonmail.com>
2023-10-09 15:00:20 -0500

    add obsidian-nvim

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -14,4 +14,23 @@ return {
         end
     },
 
+    {
+        "epwalsh/obsidian.nvim",
+        lazy = true,
+        event = {
+            -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+            -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+            "BufReadPre path/to/my-vault/**.md",
+            "BufNewFile path/to/my-vault/**.md",
+        },
+        dependencies = {
+            -- Required.
+            "nvim-lua/plenary.nvim",
+            "hrsh7th/nvim-cmp",
+            "nvim-telescope/telescope.nvim",
+            "ibhagwan/fzf-lua",
+            "junegunn/fzf",
+        },
+    }
+
 }
