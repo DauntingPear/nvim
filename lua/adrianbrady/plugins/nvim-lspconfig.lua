f0a606150536a0228551877052f197bd2113408d
Adrian Brady <ajbrady999@protonmail.com>
2024-06-11 11:42:16 -0500

    feat: add keybind for signature help

--- lua/adrianbrady/plugins/nvim-lspconfig.lua
+++ lua/adrianbrady/plugins/nvim-lspconfig.lua
@@ -86,6 +86,8 @@ return {
           --  Most Language Servers support renaming across files, etc.
           map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
 
+          map('<C-k>', vim.lsp.buf.signature_help, 'Signature Help')
+
           -- Execute a code action, usually your cursor needs to be on top of an error
           -- or a suggestion from your LSP for this to activate.
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
